457903bb752e7304fe8f6fafdd4197c82c967924
SrTibii <marcluengoferra@gmail.com>
2026-03-12 13:21:22 +0100

    Enemigo Barra vida UI
    (esta en oculto pero programado)

--- SuperBusEMT/Assets/Scripts/Enemigo/Enemigo.cs
+++ SuperBusEMT/Assets/Scripts/Enemigo/Enemigo.cs
@@ -1,28 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
 
 public class Enemigo : MonoBehaviour
 {
     public int vidaActualEnemigo = 100;
+    public int vidaEnemigoMaxima = 100;
 
-    [SerializeField] private PlayerVida playerVida; // Referencia al script PlayerVida para acceder a la vida del player
+    //[Header("Interfaz Vida")]
+    //public Image barraSalud; // Arrastra aquí la imagen "Fill" del canvas hijo
+    //public TextMeshProUGUI vidaText; // Opcional
+
+    [SerializeField] private PlayerVida playerVida;
+
+    private void Start()
+    {
+        //vidaActualEnemigo = vidaEnemigoMaxima;
+        //ActualizarInterfazVida();
+    }
 
     public void RecibirDanoEnemigo(int cantidadDano)
     {
-     
-        vidaActualEnemigo -= cantidadDano; //Se va restando la vida
-        playerVida.AumentarCombo(); // Aumentar el combo del player al dañar al enemigo
-        
+        vidaActualEnemigo -= cantidadDano;
+        playerVida.AumentarCombo();
+        //ActualizarInterfazVida();
+
         if (vidaActualEnemigo <= 0)
         {
             MorirEnemigo();
         }
     }
 
+    //void ActualizarInterfazVida()
+    //{
+    //    if (barraSalud != null)
+    //        barraSalud.fillAmount = (float)vidaActualEnemigo / vidaEnemigoMaxima;
+
+    //    if (vidaText != null)
+    //        vidaText.text = vidaActualEnemigo.ToString();
+    //}
+
     public void MorirEnemigo()
     {
-        Destroy(this.gameObject);
+        Destroy(gameObject);
         Debug.Log("Enemigo Muerto");
     }
 }
